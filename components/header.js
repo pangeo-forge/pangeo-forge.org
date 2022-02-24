63a98cfe4090b176b4f01a56042a833a3ba343ae
Joseph Hamman <jhamman@ucar.edu>
2022-02-23 20:04:15 -0800

    workmark links to home page

--- components/header.js
+++ components/header.js
@@ -5,15 +5,17 @@ import { Box, Flex } from 'theme-ui'
 const Header = ({ menuItems }) => {
   return (
     <Flex sx={{ mt: [3], mb: [3] }}>
-      <Box sx={{ flex: '1 1 auto' }}>
-        {/* TODO: replace with vector version */}
-        <Image
-          src='https://pangeo-forge.org/img/pangeo-forge-text-only-white.042935a8.png'
-          alt='Pangeo-Forge workdmark'
-          width={224}
-          height={28}
-        />
-      </Box>
+      <Link href={'/'} passHref>
+        <Box sx={{ flex: '1 1 auto' }}>
+          {/* TODO: replace with vector version */}
+          <Image
+            src='https://pangeo-forge.org/img/pangeo-forge-text-only-white.042935a8.png'
+            alt='Pangeo-Forge workdmark'
+            width={224}
+            height={28}
+          />
+        </Box>
+      </Link>
       {Object.keys(menuItems).map((key, i) => (
         <Link key={i} href={menuItems[key]} passHref>
           <Box
