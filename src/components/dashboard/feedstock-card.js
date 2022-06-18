9fb27f0161ed67b46a58cb831d07ac5595d62b4d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-17 22:13:01 -0600

    use spinner

--- src/components/dashboard/feedstock-card.js
+++ src/components/dashboard/feedstock-card.js
@@ -5,10 +5,10 @@ import {
   Heading,
   LinkBox,
   LinkOverlay,
-  Skeleton,
   Stack,
   Text,
   VStack,
+  Spinner,
 } from '@chakra-ui/react'
 import React from 'react'
 import { GoMarkGithub } from 'react-icons/go'
@@ -19,6 +19,16 @@ export const FeedstockCard = ({ spec, id }) => {
   const { repo, repoError } = useRepo(spec)
 
   if (repoError) return <Box>{'error'}</Box>
+  if (!repo)
+    return (
+      <Spinner
+        thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='blue.500'
+        size='xl'
+      />
+    )
 
   return (
     <LinkBox
@@ -34,38 +44,36 @@ export const FeedstockCard = ({ spec, id }) => {
         boxShadow: 'var(--chakra-shadows-md)',
       }}
     >
-      <Skeleton isLoaded={repo}>
-        <VStack spacing={2} align='stretch'>
-          <LinkOverlay href={href}>
-            <VStack spacing={2} align='stretch'>
-              <Heading as={'h3'} size='xs' textTransform='uppercase'>
-                {spec
-                  .toLowerCase()
-                  .replace('pangeo-forge/', '')
-                  .replace('-feedstock', '')}
-              </Heading>
-              <Text lineHeight='tall' opacity={0.8}>
-                {repo.commit.message}
+      <VStack spacing={2} align='stretch'>
+        <LinkOverlay href={href}>
+          <VStack spacing={2} align='stretch'>
+            <Heading as={'h3'} size='xs' textTransform='uppercase'>
+              {spec
+                .toLowerCase()
+                .replace('pangeo-forge/', '')
+                .replace('-feedstock', '')}
+            </Heading>
+            <Text lineHeight='tall' opacity={0.8}>
+              {repo.commit.message}
+            </Text>
+            <Stack justify={'right'} direction='row' spacing={4}>
+              <Text
+                _dark={{ color: 'teal.400' }}
+                fontSize='xs'
+                fontWeight='bold'
+                whiteSpace='break-spaces'
+                variant='outline'
+              >
+                {TimeDeltaFormatter(
+                  Date.now() - Date.parse(repo.commit.committer.date)
+                )}{' '}
+                via
               </Text>
-              <Stack justify={'right'} direction='row' spacing={4}>
-                <Text
-                  _dark={{ color: 'teal.400' }}
-                  fontSize='xs'
-                  fontWeight='bold'
-                  whiteSpace='break-spaces'
-                  variant='outline'
-                >
-                  {TimeDeltaFormatter(
-                    Date.now() - Date.parse(repo.commit.committer.date)
-                  )}{' '}
-                  via
-                </Text>
-                <GoMarkGithub />
-              </Stack>
-            </VStack>
-          </LinkOverlay>
-        </VStack>
-      </Skeleton>
+              <GoMarkGithub />
+            </Stack>
+          </VStack>
+        </LinkOverlay>
+      </VStack>
     </LinkBox>
   )
 }
