9932d0ba6110381738c8de132749b4b0ef5ea600
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-19 16:13:02 -0600

    Fix recipe runs page

--- src/pages/dashboard/recipe-runs.js
+++ src/pages/dashboard/recipe-runs.js
@@ -1,28 +1,49 @@
+import { RecipeRunCard } from '@/components/dashboard'
 import { Layout } from '@/components/layout'
-import RecipeRunCard from '@/components/recipe-run-card'
 import { useRecipeRuns } from '@/lib/endpoints'
-import { Box } from 'theme-ui'
+import { Box, Container, SimpleGrid, Heading, VStack } from '@chakra-ui/react'
 
 const RecipeRuns = () => {
   const { recipeRuns, recipeRunsError } = useRecipeRuns()
 
   if (recipeRunsError) {
     return (
-      <Layout container={true} menu={true}>
+      <Layout>
         <Box>Failed to load...</Box>
       </Layout>
     )
   }
-  if (!recipeRuns) return <Layout container={true} menu={true} />
+  if (!recipeRuns) return <Layout />
 
   return (
-    <Layout container={true} menu={true}>
-      <Box>
-        <Box>
-          {recipeRuns.reverse().map((b, i) => (
-            <RecipeRunCard key={i} props={b} />
-          ))}
-        </Box>
+    <Layout>
+      <Box as='section'>
+        <Container maxW='container.xl' py={90} centerContent>
+          <VStack p={8}>
+            <Heading as={'h1'} mb={2}>
+              Recipe Runs
+            </Heading>
+          </VStack>
+
+          <SimpleGrid columns={{ base: 1, md: 1, lg: 1 }} spacing={2}>
+            {recipeRuns
+              .sort((a, b) => a.recipe_id.localeCompare(b.recipe_id))
+              .sort((a, b) => a.started_at.localeCompare(b.started_at))
+
+              .reverse()
+              .map((recipe, index) => (
+                <RecipeRunCard
+                  key={index}
+                  recipe_id={recipe.recipe_id}
+                  id={recipe.id}
+                  started_at={recipe.started_at}
+                  status={recipe.status}
+                  version={recipe.version}
+                  message={recipe.message}
+                />
+              ))}
+          </SimpleGrid>
+        </Container>
       </Box>
     </Layout>
   )
