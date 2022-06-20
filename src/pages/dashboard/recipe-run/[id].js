2ec496dd3f24436e8e4a880debff9c814321eadb
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-20 01:23:22 -0600

    fix typo

--- src/pages/dashboard/recipe-run/[id].js
+++ src/pages/dashboard/recipe-run/[id].js
@@ -159,7 +159,7 @@ const RecipeRun = () => {
             <Box>
               {prefect &&
                 prefect.data?.flow_run.map((run, index) => (
-                  <Text key={index}>{JSON.stringfy(run)}</Text>
+                  <Text key={index}>{JSON.stringify(run)}</Text>
                 ))}
             </Box>
           </Box>
