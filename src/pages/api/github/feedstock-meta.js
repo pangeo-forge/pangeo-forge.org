4f60a38043527f2cf76b008487e486c30ea96139
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-07-12 10:16:03 -0600

    remove octokit dependency (#123)

--- src/pages/api/github/feedstock-meta.js
+++ src/pages/api/github/feedstock-meta.js
@@ -6,11 +6,14 @@ export default async function handler(req, res) {
   const { searchParams } = new URL(req.url)
 
   const requestURL = searchParams.get('path')
+
   const headers = {
     Accept: 'application/vnd.github.v3+json',
-    Authorization: `token ${process.env.GITHUB_TOKEN}`,
     //'cache-control': 'public, s-maxage=600, stale-while-revalidate=300',
   }
+
+  if (process.env.GITHUB_TOKEN)
+    headers['Authorization'] = `token ${process.env.GITHUB_TOKEN}`
 
   try {
     const result = await fetch(requestURL, { headers })
