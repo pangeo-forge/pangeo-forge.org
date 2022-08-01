7f130ddaf07ad6e9e483c651c97f14207e560d97
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-08-01 17:57:21 -0600

    update json fetcher

--- src/lib/fetchers.js
+++ src/lib/fetchers.js
@@ -1,6 +1,19 @@
 import yaml from 'js-yaml'
 
-export const jsonFetcher = (url) => fetch(url).then((r) => r.json())
+export const jsonFetcher = async (url) => {
+  const response = await fetch(url)
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    error.info = await response.json()
+    error.status = response.status
+    throw error
+  }
+
+  return response.json()
+}
 export const yamlFetcher = (url) =>
   fetch(url)
     .then((res) => res.blob())
