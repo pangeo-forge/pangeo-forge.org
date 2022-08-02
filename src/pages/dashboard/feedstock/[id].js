503c39d0dfbabf70026eec7db3e02fd3c0108d79
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-08-02 17:52:08 -0600

    update loading state logic

--- src/pages/dashboard/feedstock/[id].js
+++ src/pages/dashboard/feedstock/[id].js
@@ -28,7 +28,7 @@ const Feedstock = () => {
   const {
     fs: { spec = '', recipe_runs = [] } = {},
     fsError,
-    isLoading: fsIsLoading,
+    isLoading: feedstockIsLoading,
   } = useFeedstock(id)
   const { meta, metaError, isLoading: metaIsLoading } = useMeta(spec)
 
@@ -43,53 +43,51 @@ const Feedstock = () => {
       </Layout>
     )
 
-  if (fsIsLoading || metaIsLoading)
-    return (
-      <Layout>
-        <Skeleton minH={'100vh'}></Skeleton>
-      </Layout>
-    )
-
-  const name = getName(spec)
   const { isProduction, datasets } = getProductionRunInfo(id, recipe_runs)
 
   return (
     <Layout>
       <Box as='section'>
         <Container maxW='container.xl' mt={90}>
-          <FeedstockInfo
-            repo={spec}
-            name={name}
-            title={meta.title}
-            description={meta.description}
-            pangeo_forge_version={meta.pangeo_forge_version}
-            pangeo_notebook_version={meta.pangeo_notebook_version}
-            bakery={meta.bakery?.id}
-            license={meta.provenance?.license}
-            providers={meta.provenance?.providers}
-            maintainers={meta?.maintainers}
-          />
+          <Skeleton isLoaded={!metaIsLoading}>
+            <FeedstockInfo
+              repo={spec ? spec : ''}
+              name={spec ? getName(spec) : ''}
+              title={meta?.title}
+              description={meta?.description}
+              pangeo_forge_version={meta?.pangeo_forge_version}
+              pangeo_notebook_version={meta?.pangeo_notebook_version}
+              bakery={meta?.bakery?.id}
+              license={meta?.provenance?.license}
+              providers={meta?.provenance?.providers}
+              maintainers={meta?.maintainers}
+            />
+          </Skeleton>
 
           <Tabs isLazy isFitted colorScheme='teal' my={16}>
             <TabList>
               <Tab>Recipe Runs</Tab>
               <Tab>
                 <GoDatabase />
-                <Text ml={2}>{`${datasets.length} Dataset${
-                  datasets.length > 1 ? 's' : ''
+                <Text ml={2}>{`${datasets?.length} Dataset${
+                  datasets?.length > 1 ? 's' : ''
                 }`}</Text>
               </Tab>
             </TabList>
             <TabPanels>
               <TabPanel>
                 {' '}
-                <RecipeRuns runs={recipe_runs} />
+                <Skeleton isLoaded={!feedstockIsLoading}>
+                  <RecipeRuns runs={recipe_runs} />
+                </Skeleton>
               </TabPanel>
               <TabPanel>
-                <FeedstockDatasets
-                  isProduction={isProduction}
-                  datasets={datasets}
-                />
+                <Skeleton isLoaded={!feedstockIsLoading}>
+                  <FeedstockDatasets
+                    isProduction={isProduction}
+                    datasets={datasets}
+                  />
+                </Skeleton>
               </TabPanel>
             </TabPanels>
           </Tabs>
